4c07f7da26142c76522820f506f5cb0ac3cdb7c6
BrennerG <gabrielbrbr@gmail.com>
2022-05-13 18:04:55 +0200

    data

---
+++ src/data_access/csqa_dataset.py
@@ -0,0 +1,39 @@
+import json
+import pandas as pd
+import torch
+from torch.utils.data import Dataset
+
+# TODO would transforms be useful here? (e.g. transforming to BERT, masking, ...)
+class CsqaDataset(Dataset):
+
+    def __init__(self, path_to_raw:str, limit=-1):
+        self.data = []
+
+        with open(path_to_raw, 'r') as json_file:
+            json_list = list(json_file)
+
+            #for json_str in json_list:
+            if limit > 0:
+                for json_str in json_list[:limit]:
+                    result = json.loads(json_str)
+                    self.data.append(result)
+            else:
+                for json_str in json_list:
+                    result = json.loads(json_str)
+                    self.data.append(result)
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        question = self.data[idx]['question']['stem']
+        answer = [x['text'] for x in self.data[idx]['question']['choices']]
+        context = self.data[idx]['question']['question_concept']
+        label_map = {'A':0, 'B':1, 'C':2, 'D':3, 'E':4}
+        label = label_map[self.data[idx]['answerKey']]
+        #label_tensor = torch.zeros(5)
+        #label_tensor[label] = 1
+        return question, context, answer, float(label)
+
+
+# if __name__ == "__main__":
---
+++ src/data_access/locations.py
@@ -0,0 +1,7 @@
+LOC = {
+    'csqa_train': 'data/csqa_raw/train_rand_split.jsonl',
+    'models_dir': 'data/models/',
+    'experiments_dir': 'data/experiments/',
+    'preprocessed_data': 'data/csqa_preprocessed/',
+    'predictions': 'data/predicted/'
+}
