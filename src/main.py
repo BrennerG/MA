6c3525fc9fe458a657dca62123f9e639ee7b41ac
Gabriel Breiner <gbreiner@troubadix.ifs.tuwien.ac.at>
2022-08-17 12:05:10 +0200

    RUN sage-smoke-18: roberta lr=1e-5

--- src/main.py
+++ src/main.py
@@ -21,15 +21,15 @@ PARAMS = {
     #'skip_evaluation': True, # skip evaluation (prediction still happens...)
     'use_cuda': True, # use cuda
     'rnd_seed': 69, # Random obligatory 
-    'bert_base': 'albert-base-v2', # choose the pretrained bert-base
+    'bert_base': 'roberta-base', # choose the pretrained bert-base
     'attention': 'lime', # how to generate token weights (relevant for explainability metrics) {'lime', 'zeros', 'random', None}
     'lime_num_features': 10, # number of tokens that lime assigns a weight to
     'lime_num_permutations': 20, # number of input permutations per sample; default=5k :O
-    'learning_rate': 9e-6, # Optimizer learning rate
+    'learning_rate': 1e-5, # Optimizer learning rate
     'batch_size': 16, # batch size for training and evaluation
     'epochs': 10, # epochs of training
     'save_strategy': 'epoch', # determines rules for saving artifacts {'no', 'epoch', 'steps'}
-    'save_loc': 'data/experiments/bert10_lr=9e-6/', # location where the above are saved
+    'save_loc': 'data/experiments/roberta10_lr=1e-5/', # location where the above are saved
     'overwrite_output_dir': True, # overwrites the given output directory above
 }
 
