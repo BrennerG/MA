6aef5538a88ed82306bea099f88a82323b4e4a3f
Gabriel Breiner <gbreiner@troubadix.ifs.tuwien.ac.at>
2022-08-17 15:00:14 +0200

    RUN EVAL: curious-darkness_notrain_lime_perm=5

--- src/main.py
+++ src/main.py
@@ -24,12 +24,12 @@ PARAMS = {
     'bert_base': 'albert-base-v2', # choose the pretrained bert-base
     'attention': 'lime', # how to generate token weights (relevant for explainability metrics) {'lime', 'zeros', 'random', None}
     'lime_num_features': 10, # number of tokens that lime assigns a weight to
-    'lime_num_permutations': 10, # number of input permutations per sample; default=5k :O
+    'lime_num_permutations': 5, # number of input permutations per sample; default=5k :O
     'learning_rate': 9e-6, # Optimizer learning rate
     'batch_size': 16, # batch size for training and evaluation
     'epochs': 10, # epochs of training
     'save_strategy': 'epoch', # determines rules for saving artifacts {'no', 'epoch', 'steps'}
-    'save_loc': 'data/experiments/curious-darkness_notrain_lime_perm=10/', # location where the above are saved
+    'save_loc': 'data/experiments/curious-darkness_notrain_lime_perm=5/', # location where the above are saved
     'overwrite_output_dir': True, # overwrites the given output directory above
 }
 
