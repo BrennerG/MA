0d4814efafa4e6ba7dbd32024a930e673a2a370f
Gabriel Breiner <gabriel.breiner@protonmail.com>
2022-10-03 11:18:36 +0200

    TODO bert_experiment.py: rm TODO

--- src/experiments/bert_experiment.py
+++ src/experiments/bert_experiment.py
@@ -14,8 +14,6 @@ from tqdm import tqdm
 import wandb
 import os
 
-# TODO fix softmax_logits (==> softmax should be deactivated for training and eval purposes (backpropagation!!!), 
-#       but is needed for ERASER metrics! (so check for this and softmax in eval_util.py!)
 # TODO test this online (re-run c.dark and verify nothing has changed...!)
 
 class BERTExperiment(Experiment):
