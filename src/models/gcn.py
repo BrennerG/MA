67afcf71bc958b46dea3bbcaaeef91b5c7591f09
Gabriel Breiner <gabriel.breiner@protonmail.com>
2022-10-04 14:56:07 +0200

    MOD experiment.py + gcn.py: adjusted for changes in parameter passing

--- src/models/gcn.py
+++ src/models/gcn.py
@@ -9,13 +9,15 @@ from data.locations import LOC
 class GCN(torch.nn.Module):
     def __init__(self, params:{}):
         super().__init__()
-        assert 'gcn_hidden_dim' in params
+        assert 'input_dims' in params
+        gcn_hidden_dim = params['input_dims'][0]
         self.device = 'cuda:0' if ('use_cuda' in params and params['use_cuda']) else 'cpu'
         self.embedding= GloveEmbedder(params, LOC['glove_embedding'])
-        self.conv1 = GCNConv(self.embedding.dim, params['gcn_hidden_dim'])
-        self.conv2 = GCNConv(params['gcn_hidden_dim'],1)
+        self.conv1 = GCNConv(self.embedding.dim, gcn_hidden_dim)
+        self.conv2 = GCNConv(gcn_hidden_dim,1)
 
-    def forward(self, data): # TODO assert only for single sample use
+    def forward(self, data): 
+        assert isinstance(data,dict)
         proba_vec = torch.zeros(5)
         for i,answer in enumerate(data['answers']):
             qa = f"{data['question']} {answer}"
@@ -25,9 +27,8 @@ class GCN(torch.nn.Module):
             x = F.relu(x)
             x = F.dropout(x, training=self.training)
             x = self.conv2(x, edge_index)
-            # pooling # TODO experiment
             proba_vec[i] = x.mean(dim=0) # same as torch_geometric.nn.pool.glob.global_mean_pool
-        return F.log_softmax(proba_vec, dim=0), None # TODO Cross EntropyLoss expects true logits - not softmax!
+        return F.log_softmax(proba_vec, dim=0), None 
     
-    def __call__(self, sample):
+    def __call__(self, sample, **args): # TODO feed args into forward as needed!
         return self.forward(sample)
